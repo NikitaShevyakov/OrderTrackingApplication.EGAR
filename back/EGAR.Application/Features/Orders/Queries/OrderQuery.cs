ab989083d10877be4842fa6bca5198113928e46c
NikitaShevyakov <developer.redcat@gmail.com>
2026-02-07 00:47:45 +0300

    test

--- back/EGAR.Application/Features/Orders/Queries/OrderQuery.cs
+++ back/EGAR.Application/Features/Orders/Queries/OrderQuery.cs
@@ -20,8 +20,9 @@ public class OrderQueryHandler : IRequestHandler<OrderQuery, Result<Order>>
     public async Task<Result<Order>> Handle(OrderQuery q, CancellationToken ct = default)
     {
         throw new Exception();
-        
-        if(q.Id <=0)
+        throw new Exception();
+
+        if (q.Id <=0)
             return Result<Order>.Failure(ErrorType.Validation, $"Parameter 'Id' must be a positive integer greater than 0. Received: {q.Id}");
 
         var order = await _orderRepository.GetByIdAsync(q.Id, ct);
